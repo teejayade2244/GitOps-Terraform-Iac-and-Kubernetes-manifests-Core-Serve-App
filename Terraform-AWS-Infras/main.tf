f168ff7adedbcf0e3e44b1652baa10c23e00c34c
teejay4125 <teejay4125@gmail.com>
2025-02-04 20:05:52 +0000

    `Removed all Terraform configuration files: main.tf, output.tf, providers.tf, and variables.tf.`

--- Terraform-AWS-Infras/main.tf
+++ Terraform-AWS-Infras/main.tf
@@ -4,11 +4,12 @@ module "vpc" {
 }
 
 module "eks" {
-  source     = "./modules/eks"
+  source     = "./modules/Eks"
   public_subnets= module.vpc.public_subnets
 }
 
 module "security_groups" {
-  source = "./modules/security_groups"
+  source = "./modules/SecurityGroups"
   vpc_id = module.vpc.vpc_id
+  cidr_block = "10.0.0.0/16"
 }
