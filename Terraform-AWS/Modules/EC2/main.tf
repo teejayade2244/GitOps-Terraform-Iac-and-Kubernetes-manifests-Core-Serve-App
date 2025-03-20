65da81577e566a8024d103c6e1f37329e8569e02
TemitopeAde22 <temitope224468@gmail.com>
2025-03-20 21:48:50 +0000

    update

--- Terraform-AWS/Modules/EC2/main.tf
+++ Terraform-AWS/Modules/EC2/main.tf
@@ -9,7 +9,6 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]  # Note: This expects a single ID, not a list
-  key_name               = "tope.pem"
   tags = {
     Name = var.server_name
   }
