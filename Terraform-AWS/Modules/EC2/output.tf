682465b99a113574fe05d92c954227a83efcf159
TemitopeAde22 <temitope224468@gmail.com>
2025-03-20 02:16:30 +0000

    update

--- Terraform-AWS/Modules/EC2/output.tf
+++ Terraform-AWS/Modules/EC2/output.tf
@@ -1,9 +1,9 @@
-output "public_ip" {
-  description = "The public IP address of the EC2 instance"
-  value       = aws_instance.ec2_instance.public_ip
-}
+# output "public_ip" {
+#   description = "The public IP address of the EC2 instance"
+#   value       = aws_instance.ec2_instance.public_ip
+# }
 
-output "private_ip" {
-  description = "The private IP address of the EC2 instance"
-  value       = aws_instance.ec2_instance.private_ip
-}
+# output "private_ip" {
+#   description = "The private IP address of the EC2 instance"
+#   value       = aws_instance.ec2_instance.private_ip
+# }
