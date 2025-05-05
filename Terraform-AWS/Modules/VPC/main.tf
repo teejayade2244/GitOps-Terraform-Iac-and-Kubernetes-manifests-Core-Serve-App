8f352875fa07bf25d157034b0715bddff36c8d48
TemitopeAde22 <temitope224468@gmail.com>
2025-05-05 23:25:28 +0100

    update

--- Terraform-AWS/Modules/VPC/main.tf
+++ Terraform-AWS/Modules/VPC/main.tf
@@ -48,7 +48,6 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-
 # associate public subnets to public route table
 resource "aws_route_table_association" "public-subnet_route_table_association" {
   for_each       = aws_subnet.public_subnets
@@ -56,7 +55,8 @@ resource "aws_route_table_association" "public-subnet_route_table_association" {
   route_table_id = aws_route_table.public_route_table.id
 }
 
-#private subnets
+############################################################################################################
+# private subnets
 resource "aws_subnet" "private_subnets" {
   for_each = var.Private_subnets
   vpc_id            = aws_vpc.main.id
@@ -67,3 +67,44 @@ resource "aws_subnet" "private_subnets" {
     Environment = var.environment
   }
 }
+
+# create route table and add private route
+resource "aws_route_table" "private_route_table" {
+  vpc_id       = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+     nat_gateway_id = aws_nat_gateway.nat_gateway.id
+    
+  }
+  tags       = {
+    Name     = "Private-rt"
+    Environment = var.environment
+  }
+}
+
+# Associate private subnets with private route table
+resource "aws_route_table_association" "private_subnet_route_table_association" {
+  for_each       = aws_subnet.private_subnets
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+# Allocate an Elastic IP for the NAT Gateway
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+# Create the NAT Gateway in the public subnet
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = values(aws_subnet.public_subnets)[0].id
+   tags = {
+    Name = "${var.project_name}-nat-gw"
+    Environment = var.environment
+  }
+  depends_on = [aws_internet_gateway.internet_gateway]
+}
+
