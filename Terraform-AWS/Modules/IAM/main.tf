045e3075b0fddecd1f512a094e11d45e4758d195
TemitopeAde22 <temitope224468@gmail.com>
2025-05-15 08:18:23 +0100

    update

--- Terraform-AWS/Modules/IAM/main.tf
+++ Terraform-AWS/Modules/IAM/main.tf
@@ -24,7 +24,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.eks_cluster_role.name
+  role       = aws_iam_role.eks-cluster-role.name
 }
 
 resource "aws_iam_role" "eks-nodegroup-role" {
@@ -46,17 +46,12 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonWorkerNodePolicy" {
   role       = aws_iam_role.eks-nodegroup-role.name
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_nodegroup_role.name
-}
-
 resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_nodegroup_role.name
+  role       = aws_iam_role.eks-nodegroup-role.name
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_nodegroup_role.name
-}
+  role       = aws_iam_role.eks-nodegroup-role.name
+} 
