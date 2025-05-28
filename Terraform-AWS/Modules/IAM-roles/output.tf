58287ce58c4b661d1500e6d5e3fac9fc737ef451
TemitopeAde22 <temitope224468@gmail.com>
2025-05-28 21:41:19 +0100

    update

--- Terraform-AWS/Modules/IAM-roles/output.tf
+++ Terraform-AWS/Modules/IAM-roles/output.tf
@@ -9,6 +9,10 @@ output "role_name" {
   description = "The name of the IAM role"
 }
 
+
 output "instance_profile_name" {
-  value = aws_iam_instance_profile.instance_profile[0].name
+  description = "The name of the IAM instance profile, if created."
+  # Use a conditional expression to only get the name if the instance profile exists.
+  # If length is 0, it means it wasn't created, so return null.
+  value       = length(aws_iam_instance_profile.instance_profile) > 0 ? aws_iam_instance_profile.instance_profile[0].name : null
 }
