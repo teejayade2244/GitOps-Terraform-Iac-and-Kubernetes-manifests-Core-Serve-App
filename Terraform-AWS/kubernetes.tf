3c16831a911425b8732efa77cea99ceaf3144052
TemitopeAde22 <temitope224468@gmail.com>
2025-05-29 20:38:53 +0100

    update

--- Terraform-AWS/kubernetes.tf
+++ Terraform-AWS/kubernetes.tf
@@ -41,16 +41,3 @@ resource "kubernetes_service_account" "aws_load_balancer_controller" {
   }
 }
 
-resource "kubernetes_service_account" "aws_load_balancer_controller" {
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.aws_load_balancer_controller.arn
-    }
-    labels = {
-      "app.kubernetes.io/component" = "controller"
-      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
-    }
-  }
-}
