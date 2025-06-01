6af090852c1e55680b8a9c9ef7d67929f4eec5fc
TemitopeAde22 <temitope224468@gmail.com>
2025-06-01 14:18:45 +0100

    update

--- Terraform-AWS/helm.tf
+++ Terraform-AWS/helm.tf
@@ -73,3 +73,46 @@ resource "helm_release" "vault" {
   wait    = true
   timeout = 600
 }
+
+
+resource "helm_release" "sealed_secrets" {
+  name             = "sealed-secrets"
+  repository       = "https://bitnami-labs.github.io/sealed-secrets"
+  chart            = "sealed-secrets"
+  namespace        = "kube-system"
+  version          = "2.13.3"
+  create_namespace = true
+
+  set {
+    name  = "fullnameOverride"
+    value = "sealed-secrets-controller"
+  }
+
+  set {
+    name  = "serviceMonitor.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "resources.requests.memory"
+    value = "128Mi"
+  }
+
+  set {
+    name  = "resources.requests.cpu"
+    value = "100m"
+  }
+
+  set {
+    name  = "resources.limits.memory"
+    value = "256Mi"
+  }
+
+  set {
+    name  = "resources.limits.cpu"
+    value = "200m"
+  }
+
+  wait    = true
+  timeout = 600
+}
